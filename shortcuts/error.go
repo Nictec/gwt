bd0b3e195a19d9f2efc40a6c5cab74269aac18c5
nl <nl@x-net.at>
2021-07-16 10:44:12 +0200

    added error function

--- shortcuts/error.go
+++ shortcuts/error.go
@@ -1,7 +1,10 @@
 package shortcuts
 
-import "github.com/Nictec/gwt/handler"
+import (
+	"github.com/Nictec/gwt/handler"
+	"github.com/flosch/pongo2/v4"
+)
 
-func Error(ctx handler.Context, code int, message string){
-
+func Error(ctx handler.Context, code int, message string) {
+	Render(ctx, "templates/error.html", pongo2.Context{"code": code, "message": message})
 }
